771b0eb6665d8536939511b9d32837fc265335f4
Geoffrey Martin <geoffm39@gmail.com>
2024-01-16 18:44:57 +1100

    create MainWindow layout

--- gui/windows/main_window.py
+++ gui/windows/main_window.py
@@ -8,4 +8,18 @@ from gui.custom_widgets.options_frame import OptionsFrame
 
 class MainWindow:
     def __init__(self, root: Tk):
-        pass
+        root.title('Typing Speed Test')
+        root.iconbitmap('gui/keyboard_icon.ico')
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        mainframe = ttk.Frame(root)
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure(1, weight=1)
+
+        self.button_frame = ttk.Frame(mainframe)
+        self.button_frame.grid(column=0, row=0, sticky=(W, E))
+
+        self.test_frame = ttk.Frame(mainframe, borderwidth=5, relief='ridge')
+        self.test_frame.grid(column=0, row=1, sticky=(N, W, E, S))
